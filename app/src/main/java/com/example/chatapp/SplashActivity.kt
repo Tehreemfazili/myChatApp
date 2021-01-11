968e4eb0817b1e3f027b38daae4415276b92e3bd
Tehreem <tehreemfazili5@gmail.com>
2021-01-11 20:14:07 +0530

    connected to firebase

---
+++ app/src/main/java/com/example/chatapp/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.chatapp
+
+import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+
+class SplashActivity : BaseActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    init {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            if(!it.isSuccessful){
+                return@addOnCompleteListener
+            }
+
+            val token = it.result
+
+        }
+
+    }
+}
